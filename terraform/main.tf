859e9693a25d150bf9a229e6a5a6fb87c40fd780
artem <artoncobaka@list.ru>
2023-10-06 19:45:15 +0300

    feat: tf generates inventory.ini

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,13 +37,14 @@ module "preemptible_server" {
 }
 
 # Создание inventory файла для ansible
-/* resource "local_file" "ansible_inventory" { */
-/*   content = templatefile("./resources/inventory.tmpl", */
-/*     { */
-/*       webapp_vm_ip_public  = module.preemptible_server.0.floating_ip, */
-/*       database_vm_ip_public      = module.preemptible_server.1.floating_ip, */
-/*       webapp_vm_ip_nat     = module.preemptible_server.1.nat_ip.0 */
-/*     } */
-/*   ) */
-/*   filename = "../ansible/inventory.ini" */
-/* } */
+resource "local_file" "ansible_inventory" {
+  content = templatefile("./resources/inventory.tmpl",
+    {
+      webapp_vm_ip_public  = module.preemptible_server.0.floating_ip,
+      database_vm_ip_public      = module.preemptible_server.1.floating_ip,
+      webapp_vm_ip_nat     = module.preemptible_server.1.nat_ip.0
+      database_vm_ip_nat     = module.preemptible_server.0.nat_ip.0
+    }
+  )
+  filename = "../ansible/inventory.ini"
+}
